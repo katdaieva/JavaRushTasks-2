042d107b2b4c7764a81660ef793f6e131e780faa
katdaeiva <atdk@ukr.net>
2019-02-09 12:00:45 +0200

    lecture 7

---
+++ task04/task0426/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.task04.task0426;
+
+/* 
+Ярлыки и числа
+*/
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String s = reader.readLine();
+        int n = Integer.parseInt(s);
+
+        if (n < 0)
+            if (n % 2 == 0)
+                System.out.println("отрицательное четное число");
+            else
+                System.out.println("отрицательное нечетное число");
+        if (n > 0)
+            if (n % 2 == 0)
+                System.out.println("положительное четное число");
+            else
+                System.out.println("положительное нечетное число");
+        if (n == 0)
+            System.out.println("ноль");
+    }
+}
